8161f09ecf2fdc4b94d18bb486f5fcbe96c2c40a
Edward Abboud <abboudedward@gmail.com>
2022-03-26 15:59:58 +0100

    completed ex2 successfully

--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -22,18 +22,73 @@ Use async/await and try/catch to handle promises.
 Try and avoid using global variables. As much as possible, try and use function 
 parameters and return values to pass data back and forth.
 ------------------------------------------------------------------------------*/
-function fetchData(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function fetchAndPopulatePokemons(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function fetchAndPopulatePokemons(data) {
+  const pokemonSelector = document.createElement('select');
+  pokemonSelector.addEventListener('change', fetchImage);
+
+  const getPokemonButton = document.createElement('button');
+  getPokemonButton.type = 'button';
+  getPokemonButton.textContent = 'Get Pokemon!';
+
+  document.body.appendChild(pokemonSelector);
+
+  getPokemonButton.addEventListener('click', () => {
+    const pokemon = data.results;
+    pokemon.forEach((monster) => {
+      const allPokemon = document.createElement('option');
+      allPokemon.value = monster.name;
+      allPokemon.text = monster.name;
+      pokemonSelector.appendChild(allPokemon);
+    });
+  });
+
+  document.body.appendChild(getPokemonButton);
 }
 
-function fetchImage(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchImage(event) {
+  const clearImg = document.querySelector('img');
+  if (clearImg) {
+    clearImg.remove();
+  }
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${event.currentTarget.value}`;
+
+  try {
+    const data = await fetchData(url);
+    const selected = document.querySelector('select');
+
+    if (selected) {
+      const pokemonImage = document.createElement('img');
+      pokemonImage.alt = 'Image of selected Pokemon';
+      pokemonImage.src = data.sprites.front_default;
+      document.body.appendChild(pokemonImage);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function main() {
-  // TODO complete this function
+async function main() {
+  try {
+    const data = await fetchData(
+      'https://pokeapi.co/api/v2/pokemon/?limit=151&offset=20'
+    );
+    fetchAndPopulatePokemons(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+window.addEventListener('load', main);
